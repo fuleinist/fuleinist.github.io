99eded905ced339892a423e83abd92b95283ed1e
Chris <fuleinist@gmail.com>
2016-12-09 10:48:46 +1100

    Update app.js

--- aframe-360-video/src/app.js
+++ aframe-360-video/src/app.js
@@ -1,16 +1,14 @@
-var vid360 = React.createClass({
+var vidapp = React.createClass({
   render: function() {
     return (
-	<div key="360vid">
-		<a-scene id="myscene">
-		  <a-assets>
-			<video crossorigin autoplay loop muted webkit-playsinline playsinline id="video1"  src="http://104.199.225.214/Tests/aframe-360-video/Space%20Engine%20in%20Immersive%20360%20Video.mp4" ></video>
-		  </a-assets>
-		   <a-videosphere src="#video1"></a-videosphere>
-		</a-scene>
-	</div>
+	<a-scene id="myscene">
+	<a-assets>
+		<video crossorigin autoplay loop muted webkit-playsinline playsinline id="video1"  src="http://104.199.225.214/Tests/aframe-360-video/Space%20Engine%20in%20Immersive%20360%20Video.mp4" ></video>
+	</a-assets>
+		<a-videosphere src="#video1"></a-videosphere>
+	</a-scene>
     );
   }
 });
 
-ReactDOM.render(<vid360 />, document.querySelector('#container'));
+ReactDOM.render(<vidapp />, document.querySelector('#container'));

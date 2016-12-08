ab93abf4b2b4793177a535cb50078674add2a807
Chris <fuleinist@gmail.com>
2016-12-09 10:36:26 +1100

    add react version

---
+++ aframe-360-video/src/app.js
@@ -0,0 +1,14 @@
+var vid360 = React.createClass({
+  render: function() {
+    return (
+		<a-scene id="myscene">
+		  <a-assets>
+			<video crossorigin autoplay loop muted webkit-playsinline playsinline id="video1"  src="http://104.199.225.214/Tests/aframe-360-video/Space%20Engine%20in%20Immersive%20360%20Video.mp4" ></video>
+		  </a-assets>
+		   <a-videosphere src="#video1"></a-videosphere>
+		</a-scene>
+    );
+  }
+});
+
+ReactDOM.render(<vid360 />, document.querySelector('#container'));

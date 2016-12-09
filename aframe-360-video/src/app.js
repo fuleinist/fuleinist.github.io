fc87ecc40856d13a6382736910eb69a15128c3d5
Chris <fuleinist@gmail.com>
2016-12-09 11:10:56 +1100

    Update app.js

--- aframe-360-video/src/app.js
+++ aframe-360-video/src/app.js
@@ -4,7 +4,7 @@ var vid360 = React.createClass({
 		<div key="vid360">
 			<a-scene id="myscene">
 			  <a-assets>
-				<video crossorigin autoplay loop muted webkit-playsinline playsinline id="video1"  src="http://104.199.225.214/Tests/aframe-360-video/Space%20Engine%20in%20Immersive%20360%20Video.mp4" ></video>
+				<video crossorigin autoplay loop muted webkit-playsinline playsinline id="video1"  src="ClashofClans360.mp4" ></video>
 			  </a-assets>
 			   <a-videosphere src="#video1"></a-videosphere>
 			</a-scene>

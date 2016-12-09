329921a3593cf5ae965cf025566cc8fc5dd31f5d
Chris <fuleinist@gmail.com>
2016-12-09 11:14:57 +1100

    Update app.js

--- aframe-360-video/src/app.js
+++ aframe-360-video/src/app.js
@@ -1,14 +1,15 @@
 var vid360 = React.createClass({
   render: function() {
-    return (
-		<div key="vid360">
+    var dom =  ( <div key="vid360">
 			<a-scene id="myscene">
 			  <a-assets>
 				<video crossorigin autoplay loop muted webkit-playsinline playsinline id="video1"  src="ClashofClans360.mp4" ></video>
 			  </a-assets>
 			   <a-videosphere src="#video1"></a-videosphere>
 			</a-scene>
-		</div>
+		</div> )
+    return (
+	dom
     );
   }
 });
